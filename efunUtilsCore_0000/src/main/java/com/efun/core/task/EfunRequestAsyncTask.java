44451f3b104d1ea371df67d64e178c5a4dcf75e4
ganyuanrong <ganyuanrong@gmail.com>
2016-12-27 13:45:45 +0800

    util 5.4 合并日志上报功能

--- efunUtilsCore_0000/src/main/java/com/efun/core/task/EfunRequestAsyncTask.java
+++ efunUtilsCore_0000/src/main/java/com/efun/core/task/EfunRequestAsyncTask.java
@@ -150,7 +150,7 @@ public abstract class EfunRequestAsyncTask extends AsyncTask<String, Integer, St
 	protected void onPreExecute() {
 		super.onPreExecute();
 		Log.d(LOG_TAG, "onPreExecute");
-		Log.d("efun", "efun-util.jar 5.3.2,http请求拼接accesstoken");
+		Log.d("efun", "efun-util.jar 5.4,合并日志上报功能");
 		if (!asyncExcute) {
 			throw new RuntimeException("please use asyncExcute() instead of execute()");
 		}
