5b12c193fe7d8f39ca81bad63d58b47f4a4f0fb3
ganyuanrong <ganyuanrong@efun.com>
2017-04-18 19:11:20 +0800

    在baseActivity为每个activity设置一次language   更新jar

--- StartpySDK/src/main/java/com/starpy/sdk/login/BaseLoginActivity.java
+++ StartpySDK/src/main/java/com/starpy/sdk/login/BaseLoginActivity.java
@@ -1,6 +1,9 @@
 package com.starpy.sdk.login;
 
+import android.os.Bundle;
+
 import com.core.base.SBaseActivity;
+import com.starpy.base.utils.Localization;
 
 /**
  * Created by Efun on 2017/2/7.
@@ -8,5 +11,15 @@ import com.core.base.SBaseActivity;
 
 public class BaseLoginActivity extends SBaseActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Localization.updateSGameLanguage(this);
+    }
 }
--- libraries-src/LibConnector/src/main/java/com/starpy/base/utils/Localization.java
+++ libraries-src/LibConnector/src/main/java/com/starpy/base/utils/Localization.java
@@ -1,8 +1,9 @@
 package com.starpy.base.utils;
 
-import android.app.Activity;
+import android.content.Context;
 
 import com.core.base.utils.ApkInfoUtil;
+import com.core.base.utils.SStringUtil;
 import com.starpy.base.bean.SGameLanguage;
 import com.starpy.base.cfg.ResConfig;
 
@@ -14,27 +15,42 @@ import java.util.Locale;
 
 public class Localization {
 
-    public static void gameLanguage(Activity activity, SGameLanguage gameLanguage){
+    public static void gameLanguage(Context context, SGameLanguage gameLanguage){
         if (gameLanguage == null){
             gameLanguage = SGameLanguage.zh_TW;
         }
-        if (!gameLanguage.getLanguage().equals(ResConfig.getGameLanguage(activity))){//如果语言改变，从新更新服务条款
-            StarPyUtil.saveSdkLoginTerms(activity,"");
+        if (!gameLanguage.getLanguage().equals(ResConfig.getGameLanguage(context))){//如果语言改变，从新更新服务条款
+            StarPyUtil.saveSdkLoginTerms(context,"");
         }
-        ResConfig.saveGameLanguage(activity,gameLanguage.getLanguage());
+        ResConfig.saveGameLanguage(context,gameLanguage.getLanguage());
 
         if (gameLanguage == SGameLanguage.zh_CH){
 
-            ApkInfoUtil.updateConfigurationLocale(activity, Locale.SIMPLIFIED_CHINESE);//简体
+            ApkInfoUtil.updateConfigurationLocale(context, Locale.SIMPLIFIED_CHINESE);//简体
 
         }else if(gameLanguage == SGameLanguage.en_US){
 
-            ApkInfoUtil.updateConfigurationLocale(activity, Locale.US);//英文（美国）
+            ApkInfoUtil.updateConfigurationLocale(context, Locale.US);//英文（美国）
 
         }else{
 
-            ApkInfoUtil.updateConfigurationLocale(activity, Locale.TRADITIONAL_CHINESE);//繁体
+            ApkInfoUtil.updateConfigurationLocale(context, Locale.TRADITIONAL_CHINESE);//繁体
 
         }
+    }
+
+    public static SGameLanguage getSGameLanguage(Context context){
+        String language = ResConfig.getGameLanguage(context);
+        if (SStringUtil.isEqual(SGameLanguage.en_US.getLanguage(), language)){
+            return SGameLanguage.en_US;
+        }
+        if (SStringUtil.isEqual(SGameLanguage.zh_CH.getLanguage(), language)){
+            return SGameLanguage.zh_CH;
+        }
+        return SGameLanguage.zh_TW;//默认设置为繁体中文
+    }
+
+    public static void updateSGameLanguage(Context context){
+        gameLanguage(context,getSGameLanguage(context));
     }
 }
