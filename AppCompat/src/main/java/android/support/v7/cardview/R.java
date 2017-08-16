14ab73861b747c793a28ae1d9e18d984127ce15f
ganyuanrong <372129081@qq.com>
2017-08-16 16:41:16 +0800

    添加recycile view v7 jar和资源

---
+++ AppCompat/src/main/java/android/support/v7/cardview/R.java
@@ -0,0 +1,77 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package android.support.v7.cardview;
+
+public final class R {
+    public R() {
+    }
+
+    public static final class styleable {
+        public static final int[] CardView = android.support.v7.appcompat.R.styleable.CardView;
+        public static final int CardView_android_minHeight = android.support.v7.appcompat.R.styleable.CardView_android_minHeight;
+        public static final int CardView_android_minWidth = android.support.v7.appcompat.R.styleable.CardView_android_minWidth;
+        public static final int CardView_cardBackgroundColor = android.support.v7.appcompat.R.styleable.CardView_cardBackgroundColor;
+        public static final int CardView_cardCornerRadius = android.support.v7.appcompat.R.styleable.CardView_cardCornerRadius;
+        public static final int CardView_cardElevation = android.support.v7.appcompat.R.styleable.CardView_cardElevation;
+        public static final int CardView_cardMaxElevation = android.support.v7.appcompat.R.styleable.CardView_cardMaxElevation;
+        public static final int CardView_cardPreventCornerOverlap = android.support.v7.appcompat.R.styleable.CardView_cardPreventCornerOverlap;
+        public static final int CardView_cardUseCompatPadding = android.support.v7.appcompat.R.styleable.CardView_cardUseCompatPadding;
+        public static final int CardView_contentPadding = android.support.v7.appcompat.R.styleable.CardView_contentPadding;
+        public static final int CardView_contentPaddingBottom = android.support.v7.appcompat.R.styleable.CardView_contentPaddingBottom;
+        public static final int CardView_contentPaddingLeft = android.support.v7.appcompat.R.styleable.CardView_contentPaddingLeft;
+        public static final int CardView_contentPaddingRight = android.support.v7.appcompat.R.styleable.CardView_contentPaddingRight;
+        public static final int CardView_contentPaddingTop = android.support.v7.appcompat.R.styleable.CardView_contentPaddingTop;
+
+        public styleable() {
+        }
+    }
+
+    public static final class style {
+        public static final int Base_CardView = android.support.v7.appcompat.R.style.Base_CardView;
+        public static final int CardView = android.support.v7.appcompat.R.style.CardView;
+        public static final int CardView_Dark = android.support.v7.appcompat.R.style.CardView_Dark;
+        public static final int CardView_Light = android.support.v7.appcompat.R.style.CardView_Light;
+
+        public style() {
+        }
+    }
+
+    public static final class dimen {
+        public static final int cardview_compat_inset_shadow = android.support.v7.appcompat.R.dimen.cardview_compat_inset_shadow;
+        public static final int cardview_default_elevation = android.support.v7.appcompat.R.dimen.cardview_default_elevation;
+        public static final int cardview_default_radius = android.support.v7.appcompat.R.dimen.cardview_default_radius;
+
+        public dimen() {
+        }
+    }
+
+    public static final class color {
+        public static final int cardview_dark_background = android.support.v7.appcompat.R.color.cardview_dark_background;
+        public static final int cardview_light_background = android.support.v7.appcompat.R.color.cardview_light_background;
+        public static final int cardview_shadow_end_color = android.support.v7.appcompat.R.color.cardview_shadow_end_color;
+        public static final int cardview_shadow_start_color = android.support.v7.appcompat.R.color.cardview_shadow_start_color;
+
+        public color() {
+        }
+    }
+
+    public static final class attr {
+        public static final int cardBackgroundColor = android.support.v7.appcompat.R.attr.cardBackgroundColor;
+        public static final int cardCornerRadius = android.support.v7.appcompat.R.attr.cardCornerRadius;
+        public static final int cardElevation = android.support.v7.appcompat.R.attr.cardElevation;
+        public static final int cardMaxElevation = android.support.v7.appcompat.R.attr.cardMaxElevation;
+        public static final int cardPreventCornerOverlap = android.support.v7.appcompat.R.attr.cardPreventCornerOverlap;
+        public static final int cardUseCompatPadding = android.support.v7.appcompat.R.attr.cardUseCompatPadding;
+        public static final int contentPadding = android.support.v7.appcompat.R.attr.contentPadding;
+        public static final int contentPaddingBottom = android.support.v7.appcompat.R.attr.contentPaddingBottom;
+        public static final int contentPaddingLeft = android.support.v7.appcompat.R.attr.contentPaddingLeft;
+        public static final int contentPaddingRight = android.support.v7.appcompat.R.attr.contentPaddingRight;
+        public static final int contentPaddingTop = android.support.v7.appcompat.R.attr.contentPaddingTop;
+
+        public attr() {
+        }
+    }
+}
---
+++ AppCompat/src/main/java/android/support/v7/recyclerview/R.java
@@ -0,0 +1,50 @@
+package android.support.v7.recyclerview;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+
+public final class R {
+    public R() {
+    }
+
+    public static final class styleable {
+        public static int[] RecyclerView = android.support.v7.appcompat.R.styleable.RecyclerView;
+        public static int RecyclerView_android_descendantFocusability = android.support.v7.appcompat.R.styleable.RecyclerView_android_descendantFocusability;
+        public static int RecyclerView_android_orientation = android.support.v7.appcompat.R.styleable.RecyclerView_android_orientation;
+        public static int RecyclerView_layoutManager = android.support.v7.appcompat.R.styleable.RecyclerView_layoutManager;
+        public static int RecyclerView_reverseLayout = android.support.v7.appcompat.R.styleable.RecyclerView_reverseLayout;
+        public static int RecyclerView_spanCount = android.support.v7.appcompat.R.styleable.RecyclerView_spanCount;
+        public static int RecyclerView_stackFromEnd = android.support.v7.appcompat.R.styleable.RecyclerView_stackFromEnd;
+
+        public styleable() {
+        }
+    }
+
+    public static final class id {
+        public static int item_touch_helper_previous_elevation = android.support.v7.appcompat.R.id.item_touch_helper_previous_elevation;
+
+        public id() {
+        }
+    }
+
+    public static final class dimen {
+        public static int item_touch_helper_max_drag_scroll_per_frame = android.support.v7.appcompat.R.dimen.item_touch_helper_max_drag_scroll_per_frame;
+        public static int item_touch_helper_swipe_escape_max_velocity = android.support.v7.appcompat.R.dimen.item_touch_helper_swipe_escape_max_velocity;
+        public static int item_touch_helper_swipe_escape_velocity = android.support.v7.appcompat.R.dimen.item_touch_helper_swipe_escape_velocity;
+
+        public dimen() {
+        }
+    }
+
+    public static final class attr {
+        public static int layoutManager = android.support.v7.appcompat.R.attr.layoutManager;
+        public static int reverseLayout = android.support.v7.appcompat.R.attr.reverseLayout;
+        public static int spanCount = android.support.v7.appcompat.R.attr.spanCount;
+        public static int stackFromEnd = android.support.v7.appcompat.R.attr.stackFromEnd;
+
+        public attr() {
+        }
+    }
+}
