f70722624f93c029b05d82164c898758ce698025
ganyuanrong <372129081@qq.com>
2017-12-12 14:26:42 +0800

    demo更新

--- StarpySdkDemo/src/main/java/com/starpy/sdk/demo/MainActivity.java
+++ StarpySdkDemo/src/main/java/com/starpy/sdk/demo/MainActivity.java
@@ -1,18 +1,31 @@
 package com.starpy.sdk.demo;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.core.base.SBaseActivity;
+import com.core.base.http.HttpRequestCore;
 import com.core.base.utils.FileUtil;
+import com.core.base.utils.ObbUtil;
 import com.core.base.utils.PL;
+import com.core.base.utils.PermissionUtil;
+import com.core.base.utils.ZipUtil;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.starpy.base.bean.SGameLanguage;
+import com.starpy.base.bean.SLoginType;
 import com.starpy.base.bean.SPayType;
 import com.starpy.base.utils.SLog;
 import com.starpy.base.utils.StarPyUtil;
@@ -21,6 +34,12 @@ import com.starpy.data.login.response.SLoginResponse;
 import com.starpy.sdk.out.ISdkCallBack;
 import com.starpy.sdk.out.IStarpy;
 import com.starpy.sdk.out.StarpyFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
 
 public class MainActivity extends SBaseActivity {
 
@@ -77,6 +96,9 @@ public class MainActivity extends SBaseActivity {
                             String timestamp = sLoginResponse.getTimestamp();
 
                             PL.i("uid:" + uid);
+                            if (sLoginResponse.getLoginType().equals(SLoginType.LOGIN_TYPE_GOOGLE)){
+                                PL.i("google login");
+                            }
 
                         }
                     }
@@ -139,9 +161,10 @@ public class MainActivity extends SBaseActivity {
                  * vipLevel：vip等级，没有就选""
                  */
                 iStarpy.cs(MainActivity.this,"level","vipLevel");
+//                testOBBCanRead();
+
             }
         });
-
 
 
         shareButton.setOnClickListener(new View.OnClickListener() {
@@ -196,7 +219,7 @@ public class MainActivity extends SBaseActivity {
         findViewById(R.id.demo_google_unlock).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                iStarpy.unlockAchievement("CggI-uz7vhQQAhAA");
+                iStarpy.unlockAchievement("CgkI75qesq4WEAIQAA");
             }
         });
         findViewById(R.id.demo_dis_cj).setOnClickListener(new View.OnClickListener() {
@@ -219,6 +242,18 @@ public class MainActivity extends SBaseActivity {
         });
 
     }
+
+    public static String postData(String urlStr, String data) {
+        HttpRequestCore requestCore = new HttpRequestCore();
+        requestCore.setPostByteData(data.getBytes());
+        requestCore.setRequestUrl(urlStr);
+        Map<String,String> m = requestCore.getHeaderMap();
+        m.put("Host","ep2.ite2.com.tw");
+        m.put("Content-Type","text/xml; charset=utf-8");
+        m.put("SOAPAction","http://tempuri.org/SendSMS");
+        return requestCore.doPost().getResult();
+    }
+
 
     @Override
     protected void onResume() {
@@ -267,5 +302,91 @@ public class MainActivity extends SBaseActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) { super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         PL.i("activity onRequestPermissionsResult");
         iStarpy.onRequestPermissionsResult(this,requestCode,permissions,grantResults);
+
+        if (PermissionUtil.verifyPermissions(grantResults)){
+            //同意授权，继续游戏
+            Log.i("Starpy","user agaree permission");
+        }else {
+            //玩家拒绝
+            Log.i("Starpy","user refuser permission");
+            refuserPermissionTips(this);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        iStarpy.onWindowFocusChanged(this,hasFocus);
+    }
+
+    private void refuserPermissionTips(final Activity activity){
+        AlertDialog alertDialog = new AlertDialog.Builder(activity)
+                .setMessage("應用需要讀寫權限才能進行更新和數據存儲，不授權無法進行遊戲，是否開啟讀寫權限?")
+                .setNegativeButton("開啟權限", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Starpy","requestPermissions_STORAGE");
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                            Log.i("Starpy","shouldShowRequestPermissionRationale true");
+                            //如果没勾选“不再询问”，向用户发起权限请求
+                            PermissionUtil.requestPermissions_STORAGE(activity,1001);
+                        }else {
+                            Log.i("Starpy","shouldShowRequestPermissionRationale false");
+
+                            //之前点击了“不再询问”，无法再次弹出权限申请框。引导去开启相应权限
+
+                            // 去应用信息
+                            Intent localIntent = new Intent();
+                            localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            if (Build.VERSION.SDK_INT >= 9) {
+                                localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                                localIntent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+                            } else if (Build.VERSION.SDK_INT <= 8) {
+                                localIntent.setAction(Intent.ACTION_VIEW);
+                                localIntent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+                                localIntent.putExtra("com.android.settings.ApplicationPkgName", activity.getPackageName());
+                            }
+                            startActivity(localIntent);
+
+                        }
+                    }
+                })
+                .setPositiveButton("退出遊戲", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //退出遊戲
+                        Log.i("Starpy","exit the game");
+                    }
+                }).create();
+        alertDialog.show();
+    }
+
+
+    public void testOBBCanRead(){
+        File obb = ObbUtil.getObbFile(this);
+        if (!obb.exists()){
+            PL.i("obb file not exists");
+            return;
+        }
+        boolean open_failed = false;
+        PL.i("Build.VERSION.SDK_INT " + Build.VERSION.SDK_INT);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(obb));
+            open_failed = false;
+//            ReadObbFile(br);
+            ZipUtil.upZipFile(obb,getFilesDir() + "/kdka/");
+            FileUtil.copyFile(obb.getAbsolutePath(),getExternalCacheDir() + "/testobbcanread.obb");
+            PL.i("ReadObbFile OK");
+        } catch (IOException e) {
+            open_failed = true;
+            PL.i("ReadObbFile NOT OK");
+            PL.i("ReadObbFile error msg -- " + e.getMessage());
+        }
+
+        if (open_failed) {
+            // request READ_EXTERNAL_STORAGE permission before reading OBB file
+//            ReadObbFileWithPermission();
+            PL.i("ReadObbFile NEED READ_EXTERNAL_STORAGE permission");
+        }
     }
 }
