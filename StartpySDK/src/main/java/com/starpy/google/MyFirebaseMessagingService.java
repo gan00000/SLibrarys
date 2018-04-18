ff7ac6dd4f5ae07754f7ffd7aea22411cd28eb47
ganyuanrong <372129081@qq.com>
2018-04-18 11:16:04 +0800

    添加firebase推送點擊事件

--- StartpySDK/src/main/java/com/starpy/google/MyFirebaseMessagingService.java
+++ StartpySDK/src/main/java/com/starpy/google/MyFirebaseMessagingService.java
@@ -1,10 +1,13 @@
 package com.starpy.google;
 
+import android.content.Intent;
+
 import com.core.base.utils.PL;
 import com.core.base.utils.SStringUtil;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.starpy.data.NotificationContent;
+import com.starpy.sdk.StarpyBroadcastReceiver;
 import com.starpy.sdk.utils.NotificationHelper;
 
 import java.util.Map;
@@ -33,9 +36,21 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             title = getPackageName() + " message";
         }
 
+        //设置清除消息的intent
+        Intent deleteIntent = new Intent(StarpyBroadcastReceiver.FLAG_PUSH_DELETE_ACTION);
+        deleteIntent.setPackage(this.getPackageName());
+        deleteIntent.putExtra(StarpyBroadcastReceiver.BroadcastReceiver_PushKey, StarpyBroadcastReceiver.PUSH_DELETE);
+
+        Intent clickIntent = new Intent(StarpyBroadcastReceiver.FLAG_PUSH_CLICK_ACTION);
+        clickIntent.setPackage(this.getPackageName());
+        clickIntent.putExtra(StarpyBroadcastReceiver.BroadcastReceiver_PushKey, StarpyBroadcastReceiver.PUSH_CLICK);
+
         NotificationContent notificationContent = new NotificationContent(getApplicationContext());
         notificationContent.setContentTitle(title);
         notificationContent.setContentText(body);
+
+        notificationContent.setDeleteIntent(deleteIntent);
+        notificationContent.setClickIntent(clickIntent);
 
         NotificationHelper notificationHelper = new NotificationHelper(getApplicationContext());
         notificationHelper.show(notificationContent);
@@ -46,4 +61,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onDeletedMessages() {
         super.onDeletedMessages();
     }
+
+
 }
--- StartpySDK/src/main/java/com/starpy/sdk/StarpyBroadcastReceiver.java
+++ StartpySDK/src/main/java/com/starpy/sdk/StarpyBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.core.base.utils.AppUtil;
 import com.core.base.utils.PL;
 import com.core.base.utils.SStringUtil;
 import com.starpy.base.utils.StarPyUtil;
@@ -14,12 +15,43 @@ import com.starpy.base.utils.StarPyUtil;
 
 public class StarpyBroadcastReceiver extends BroadcastReceiver {
 
+    public static final String BroadcastReceiver_PushKey = "BroadcastReceiver_PushKey";//intent data key
+
+    public static final String FLAG_BOOT_COMPLETEDP = "FLAG_BOOT_COMPLETEDP";//开机
+    public static final String FLAG_START_COMMON = "FLAG_START_COMMON";//正常启动
+    public static final String FLAG_CONNECTIVITY_CHANGE = "FLAG_CONNECTIVITY_CHANGE";//网络状态改变
+
+    public static final String FLAG_PUSH_CLICK_ACTION = "com.starpy.sdk.push.CLICK";//启动action
+    public static final String FLAG_PUSH_DELETE_ACTION = "com.starpy.sdk.push.DEL";//启动action
+
     public static final String INSTALL_REFERRER_ACTION = "com.android.vending.INSTALL_REFERRER";
+
+    public static final int PUSH_CLICK = 100;
+    public static final int PUSH_DELETE = 101;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         PL.i("StarpyBroadcastReceiver onReceive");
-        if (intent != null && INSTALL_REFERRER_ACTION.equals(intent.getAction())){
+
+        if (intent == null){
+            return;
+        }
+        String action = intent.getAction();
+        if (SStringUtil.isEmpty(action)){
+            return;
+        }
+
+        int extrInt = intent.getIntExtra(BroadcastReceiver_PushKey,0);
+        PL.i("extrInt:" + extrInt);
+
+        if (action.equals(FLAG_PUSH_CLICK_ACTION)){
+
+            AppUtil.startMainActivitiy(context);
+
+        }else if (action.equals(FLAG_PUSH_DELETE_ACTION)){
+
+
+        }else if (INSTALL_REFERRER_ACTION.equals(intent.getAction())){
             String referrer = intent.getStringExtra("referrer");
             PL.i("referrer:" + referrer);
             if (SStringUtil.isNotEmpty(referrer)){
