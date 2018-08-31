4931bb94b16704ec8b4d253d0e8c3f5a199263e3
ganyuanrong <372129081@qq.com>
2018-08-31 16:38:18 +0800

    compile 改为api

---
+++ StarpySdkDemo/src/main/java/com/starpy/hxcn/wxapi/WXEntryActivity.java
@@ -0,0 +1,10 @@
+package com.starpy.hxcn.wxapi;
+
+import com.starpy.thirdlib.wx.BaseWXEntryActivity;
+
+/**
+ * Created by gan on 2018/4/18.
+ */
+
+public class WXEntryActivity extends BaseWXEntryActivity {
+}
